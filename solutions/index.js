24bc0212b0511592fa1adff186802755cb583c35
Daniel <dandcalvo@gmail.com>
2024-05-24 16:29:50 -0700

    assignment

--- solutions/index.js
+++ solutions/index.js
@@ -1,5 +1,7 @@
 import { LinkedList } from './linked_list.js';
 
-const list = new LinkedList(4);
-list.append(1);
+const list = new LinkedList([1, 44, 4654, 16565.654]);
+list.append(6845);
+list.prepend('Daniel');
+
 console.log(list.toString());
--- solutions/linked_list.js
+++ solutions/linked_list.js
@@ -2,18 +2,132 @@ import { Node } from './node.js';
 
 export class LinkedList {
     constructor(value = null) {
-        this.head = new Node(value);
-        this.tail = this.head;
+        if (Array.isArray(value)) {
+            value = [...value];
+        } else {
+            value = [value];
+        }
+
+        this.#head = new Node();
+        this.#tail = this.head;
+        this.#size = 0;
+
+        value.forEach((element) => {
+            this.append(element);
+        });
+
+        this.#head = this.head.next;
     }
 
     append(value) {
         this.tail.next = new Node(value);
-        this.tail = this.tail.next;
+        this.#tail = this.tail.next;
+        this.#size = this.size + 1;
     }
 
     prepend(value) {
         let temp = new Node(value, this.head);
-        this.head = temp;
+        this.#head = temp;
+        this.#size = this.size + 1;
+    }
+
+    get size() {
+        return this.length;
+    }
+
+    set #size(newLength) {
+        this.length = newLength;
+    }
+
+    get head() {
+        return this.headNode;
+    }
+
+    set #head(newHead) {
+        this.headNode = newHead;
+    }
+
+    get tail() {
+        return this.tailNode;
+    }
+
+    set #tail(newTail) {
+        this.tailNode = newTail;
+    }
+
+    at(index) {
+        if (index >= this.size) {
+            return null;
+        }
+
+        let ptr = this.head;
+
+        for (let i = 0; i < index; i++) {
+            ptr = ptr.next;
+        }
+
+        return ptr;
+    }
+
+    pop() {
+        if (!this.size) {
+            return false;
+        }
+
+        if (this.size === 1) {
+            this.#head = null;
+            this.#size = 0;
+            return true;
+        }
+
+        let ptr = this.head;
+
+        while (ptr.next.next) {
+            ptr = ptr.next;
+        }
+
+        this.#tail = ptr;
+        this.tail.next = null;
+        this.#size = this.size - 1;
+        return true;
+    }
+
+    contains(value) {
+        if (!this.size) {
+            return false;
+        }
+
+        let ptr = this.head;
+
+        while (ptr) {
+            if (ptr.value === value) {
+                return true;
+            }
+
+            ptr = ptr.next;
+        }
+
+        return false;
+    }
+
+    find(value) {
+        if (!this.size) {
+            return false;
+        }
+
+        let ptr = this.head;
+        let index = 0;
+
+        while (ptr) {
+            if (ptr.value === value) {
+                return index;
+            }
+
+            ptr = ptr.next;
+            index++;
+        }
+
+        return null;
     }
 
     toString() {
@@ -28,4 +142,19 @@ export class LinkedList {
         result += 'null';
         return result;
     }
-}
+
+    insertAt(value, index) {
+        if (index >= this.size) {
+            return;
+        }
+
+        let ptr = this.head;
+
+        for (let i = 0; i < index - 1; i++) {
+            ptr = ptr.next;
+        }
+    }
+
+    removeAt(index) {
+
+    }
